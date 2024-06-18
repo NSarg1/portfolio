1e1dec78e901be689719d14685fffe919a10f625
Narek Sargsyan <NSarg1@hotmail.com>
2024-06-18 15:41:25 +0400

    Updates

---
+++ precache-manifest.d37f20fdd4ee9a2bb340a7567ad13d5d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0721491a50ee3409f124f89dc4e3c398",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fa499f1f5c008ff7d792",
+    "url": "/portfolio/static/css/main.659ecfdb.chunk.css"
+  },
+  {
+    "revision": "e907270b020707c32c14",
+    "url": "/portfolio/static/js/2.70395e4c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.70395e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa499f1f5c008ff7d792",
+    "url": "/portfolio/static/js/main.9c7e0d21.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "29fb182b54a49487e9b1cf62c9ee7f19",
+    "url": "/portfolio/static/media/15Puzzle.29fb182b.jpg"
+  },
+  {
+    "revision": "f0bd199b149874d18f97ea568ce9385a",
+    "url": "/portfolio/static/media/PM.f0bd199b.jpg"
+  },
+  {
+    "revision": "a3045544706da63faff8c8d21300931a",
+    "url": "/portfolio/static/media/Shopify.a3045544.jpg"
+  },
+  {
+    "revision": "15c1b2db45fe61663753942c3bfed613",
+    "url": "/portfolio/static/media/adam-min.15c1b2db.jpg"
+  },
+  {
+    "revision": "4c211ff8852df7a9ec3e439e27aa1301",
+    "url": "/portfolio/static/media/back.4c211ff8.jpg"
+  },
+  {
+    "revision": "7c7f05e5b4e14cffa5c7b1fd73e31fc4",
+    "url": "/portfolio/static/media/checkmate-checker.7c7f05e5.jpg"
+  },
+  {
+    "revision": "ca3edfbfa3cad46958b2bc371c97d4b9",
+    "url": "/portfolio/static/media/engraved-back.ca3edfbf.png"
+  },
+  {
+    "revision": "1a08480bd1d6caa74a92a8881d5989e0",
+    "url": "/portfolio/static/media/frame-perspective.1a08480b.png"
+  },
+  {
+    "revision": "6f2febe7662b7ae4b217f8dfb0731d04",
+    "url": "/portfolio/static/media/kiss-min.6f2febe7.jpg"
+  },
+  {
+    "revision": "6c106e7a410ad6bd74baf36aa89670c8",
+    "url": "/portfolio/static/media/man.6c106e7a.png"
+  },
+  {
+    "revision": "d8c5fcc766601532e0dac86d2dfc4027",
+    "url": "/portfolio/static/media/mona-min.d8c5fcc7.jpg"
+  },
+  {
+    "revision": "dd7c49a1313464c0c21987d9c9f81614",
+    "url": "/portfolio/static/media/natours.dd7c49a1.jpg"
+  },
+  {
+    "revision": "2bcc597abc8015f775a77713146b1963",
+    "url": "/portfolio/static/media/omnifood.2bcc597a.jpg"
+  },
+  {
+    "revision": "b088fa9d864d858a760f482ef5c0868c",
+    "url": "/portfolio/static/media/pizza-maker.b088fa9d.jpg"
+  },
+  {
+    "revision": "05e9a1f40148fb50bf010203c06be3b8",
+    "url": "/portfolio/static/media/react-icon.05e9a1f4.svg"
+  },
+  {
+    "revision": "88b441a144b5acc4bc2dd231eeed686a",
+    "url": "/portfolio/static/media/starry-min.88b441a1.jpg"
+  },
+  {
+    "revision": "5aba26020faf702e9c24a5f1812f3d83",
+    "url": "/portfolio/static/media/supper-min.5aba2602.jpg"
+  },
+  {
+    "revision": "a87c6a047965d3d22561c6c4c4b33486",
+    "url": "/portfolio/static/media/venus-min.a87c6a04.jpg"
+  }
+]);
